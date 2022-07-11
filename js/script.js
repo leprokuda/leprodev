c9bb2427cca1fc5b213b6e26be24db8de4939b46
leprokuda <leprokuda@gmail.com>
2022-07-11 16:07:27 +0700

    first

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-document.querySelector('.section-skills__container').onmousemove = e => {
+
+document.querySelector('.skills__container').onmousemove = e => {
 
     const x = e.pageX - e.target.offsetLeft;
     const y = e.pageY - e.target.offsetTop;
   
     e.target.style.setProperty('--x', `${x}px`);
     e.target.style.setProperty('--y', `${y}px`);
-  };
+};
 
-  function menuOnClick() {
-    document.getElementById("menu__bar").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu__background").classList.toggle("change-background");
-  }
+function menuOnClick() {
+  document.getElementById("burger__bar").classList.toggle("change-menu");
+  document.getElementById("nav").classList.toggle("change-menu");
+  document.getElementById("burger__background").classList.toggle("change-background");
+}
+
+
+
